b78e4a48fa51b9b0cf32294c8177a358577ca7b9
usman-20000 <usmanaliiali125@gmail.com>
2025-12-13 12:39:53 +0500

    msg

--- components/utils/cloudinary.js
+++ components/utils/cloudinary.js
@@ -4,9 +4,9 @@ import dotenv from "dotenv";
 dotenv.config();
 
 cloudinary.config({
-    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
-    api_key: process.env.CLOUDINARY_API_KEY,
-    api_secret: process.env.CLOUDINARY_API_SECRET,
+    cloud_name: 'duvomsgrm',
+    api_key: '752477215275183',
+    api_secret: 'JDRvRBf7jeu2Z6vjE7s_MVhvVZM',
 });
 
 export default cloudinary;
